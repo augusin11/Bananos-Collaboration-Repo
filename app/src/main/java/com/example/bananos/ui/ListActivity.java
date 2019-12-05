17061c006d82a419c245ebdb663b4be2770679a8
joeyshi12 <46363213+joeyshi12@users.noreply.github.com>
2019-12-05 02:24:14 -0800

    Changes to List page layout

--- app/src/main/java/com/example/bananos/ui/ListActivity.java
+++ app/src/main/java/com/example/bananos/ui/ListActivity.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -22,7 +21,7 @@ public class ListActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
 
-        textView = findViewById(R.id.textView2);
+        textView = findViewById(R.id.viewList);
 
         intent = getIntent();
         ArrayList<String> itemList = intent.getStringArrayListExtra("key");
